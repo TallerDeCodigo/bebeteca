d497be17f2606996690cec9395fd67d94541c74a
John <juan@losmaquiladores.com>
2014-06-19 16:45:49 -0500

    Fonts y sidebar lo mas visto

--- themes/bebeteca/sidebar.php
+++ themes/bebeteca/sidebar.php
@@ -55,9 +55,20 @@
 		</span>
 
 		<!-- Showing latest, Analytics API thing -->
-		<?php $mas_vistos = new WP_Query(array( 'posts_per_page' => 4, 'post_status'=>'publish', 'post_type' => array('post', 'articulo-slider'), 'post__not_in' => $exclude  ) );
-			if ( $mas_vistos->have_posts() ) : while( $mas_vistos->have_posts() ) : $mas_vistos->the_post(); ?>
-
+		<?php 
+			global $exclude;
+			file_put_contents(
+				'/Users/johnfalcon/Desktop/php.txt',
+				var_export( $exclude, true ) . PHP_EOL,
+				FILE_APPEND
+			);
+			$mas_vistos = new WP_Query(array( 'posts_per_page' => 4, 'post_status'=>'publish', 'post_type' => array('post', 'articulo-slider'), 'post__not_in' => $exclude  ) );
+			if ( $mas_vistos->have_posts() ) : while( $mas_vistos->have_posts() ) : $mas_vistos->the_post();
+			file_put_contents(
+				'/Users/johnfalcon/Desktop/php.txt',
+				var_export( $post->ID, true ) . PHP_EOL,
+				FILE_APPEND
+			); ?>
 			<div class="caja-ultimos">
 				<a href="<?php the_permalink(); ?>">
 					<?php the_post_thumbnail('articulos-side'); ?>
