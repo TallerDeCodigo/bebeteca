e3e1c5a59cbd7c2d279d500007e2b5256cd3079a
alejandro <alex@losmaquiladores.com>
2014-07-15 10:58:33 -0500

    relacionados side

--- themes/bebeteca/template/lado-relacionados.php
+++ themes/bebeteca/template/lado-relacionados.php
@@ -2,11 +2,17 @@
 	<span class="titulo3 verde">
 		Artículos relacionados
 	</span>
-	<?php 
+	<?php
 		global $exclude;
 		$exclude[] = $post->ID;
-		$post_general = new WP_Query(array( 'posts_per_page' => 3, 'post_status'=>'publish', 'post_type' => array('post', 'articulo-slider'), 'post__not_in' => $exclude) );
-		if ( $post_general->have_posts() ) : while( $post_general->have_posts() ) : $post_general->the_post(); 
+		$terms  = get_the_terms( $post->ID, 'category');
+		$terms_query = array();
+		foreach ($terms as $key => $term) {
+			$cat_parents = get_category_parents($term->term_id, false);
+			if( !empty($cat_parents) ) $terms_query[] = $term->term_id;
+		}
+		$post_general = new WP_Query(array( 'posts_per_page' => 3, 'post_status'=>'publish', 'post_type' => array('post', 'articulo-slider'), 'category__in' 	=> $terms_query, 'post__not_in' => $exclude, 'orderby' => 'rand') );
+		if ( $post_general->have_posts() ) : while( $post_general->have_posts() ) : $post_general->the_post();
 			$exclude[] = $post->ID;
 		?>
 			<div class="caja-ultimos">
