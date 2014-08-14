69864889a590712444dc36b27cdae8f090ed174e
alejandro <alex@losmaquiladores.com>
2014-08-14 11:42:30 -0500

    post autor

--- themes/bebeteca/author.php
+++ themes/bebeteca/author.php
@@ -12,47 +12,49 @@ $user_nicename = get_the_author_meta( 'user_displayname', $user_id);
 				<h4><?php echo $display_name; ?></h4>
 				<span class="perfil-usuario"><?php the_author_meta('perfil', $user_id) ?></span>
 			</div>
+			<?php $paged = isset($_GET['pag']) ? $_GET['pag'] : 1;
+			if ($paged == 1): ?>
+				<article class="entero autor-home clearfix single-autor">
+					<div class="cont-image">
+						<?php echo vew_image_user($user_id); ?>
+					</div>
+					<div class="sigue-colaborador">
+						<span>Contacta a este colaborador</span>
+						<ul>
+							<?php $face = get_the_author_meta( 'facebook', $user_id );
+							if ($face != ''):?>
+								<li class="fb"><a href="<?php the_author_meta('facebook', $user_id) ?>" target="_blank"></a></li>
+							<?php endif;
+							$twitter = get_the_author_meta( 'twitter', $user_id );
+							if ($twitter != ''): ?>
+								<li class="tw"><a href="<?php the_author_meta('twitter', $user_id) ?>" target="_blank"></a></li>
+							<?php endif;
+							$google = get_the_author_meta( 'googleplus', $user_id );
+							if ($google != ''):?>
+								<li class="gm"><a href="<?php the_author_meta('googleplus', $user_id) ?>" target="_blank"	></a></li>
+							<?php endif;
+							$printerest = get_the_author_meta( 'printerest', $user_id );
+							if ($printerest != ''): ?>
+								<li class="pr"><a href="<?php the_author_meta('printerest', $user_id) ?>"></a></li>
+							<?php endif;
 
-			<article class="entero autor-home clearfix single-autor">
-				<div class="cont-image">
-					<?php echo vew_image_user($user_id); ?>
-				</div>
-				<div class="sigue-colaborador">
-					<span>Contacta a este colaborador</span>
-					<ul>
-						<?php $face = get_the_author_meta( 'facebook', $user_id );
-						if ($face != ''):?>
-							<li class="fb"><a href="<?php the_author_meta('facebook', $user_id) ?>" target="_blank"></a></li>
-						<?php endif;
-						$twitter = get_the_author_meta( 'twitter', $user_id );
-						if ($twitter != ''): ?>
-							<li class="tw"><a href="<?php the_author_meta('twitter', $user_id) ?>" target="_blank"></a></li>
-						<?php endif;
-						$google = get_the_author_meta( 'googleplus', $user_id );
-						if ($google != ''):?>
-							<li class="gm"><a href="<?php the_author_meta('googleplus', $user_id) ?>" target="_blank"	></a></li>
-						<?php endif;
-						$printerest = get_the_author_meta( 'printerest', $user_id );
-						if ($printerest != ''): ?>
-							<li class="pr"><a href="<?php the_author_meta('printerest', $user_id) ?>"></a></li>
-						<?php endif;
+							$email = get_the_author_meta( 'user_email', $user_id );
+							$user_mail = get_the_author_meta( 'user_vew_mail', $user_id );
 
-						$email = get_the_author_meta( 'user_email', $user_id );
-						$user_mail = get_the_author_meta( 'user_vew_mail', $user_id );
+							if ($user_mail == 1): ?>
+								<li class="mail"><a href="mailto:<?php echo $email; ?>"></a></li>
+							<?php endif;
+							$url = get_the_author_meta( 'user_url', $user_id );
+							if ($url != ''): ?>
+								<li class="url"><a href="<?php echo $url; ?>"></a></li>
+							<?php endif; ?>
+						</ul>
+					</div>
+					<blockquote>" <?php the_author_meta('quote', $user_id) ?> "</blockquote>
+					<p><?php echo wpautop(get_the_author_meta( 'description', $user_id ) ); ?></p>
 
-						if ($user_mail == 1): ?>
-							<li class="mail"><a href="mailto:<?php echo $email; ?>"></a></li>
-						<?php endif;
-						$url = get_the_author_meta( 'user_url', $user_id );
-						if ($url != ''): ?>
-							<li class="url"><a href="<?php echo $url; ?>"></a></li>
-						<?php endif; ?>
-					</ul>
-				</div>
-				<blockquote>" <?php the_author_meta('quote', $user_id) ?> "</blockquote>
-				<p><?php echo wpautop(get_the_author_meta( 'description', $user_id ) ); ?></p>
-
-			</article>
+				</article>
+			<?php endif; ?>
 
 			<div class="entero divicion autor">
 				<span class="line"></span>
@@ -72,7 +74,7 @@ $user_nicename = get_the_author_meta( 'user_displayname', $user_id);
 				$user_nicename = get_the_author_meta( 'user_nicename', $user_id);
 				$url = site_url('/author/'.$user_nicename .'/');
 				if ($paged == 1):?>
-					<div class="boton mas-entradas right"><a href="<?php echo $url.'?pag=2'; ?>">Más entradas</a></div>
+					<div class="boton mas-entradas right"><a href="<?php echo $url.'?pag=2'; ?>">Ver Más</a></div>
 				<?php else:
 					echo paginate_links_otro($post_general->max_num_pages, $url);
 				endif; ?>
